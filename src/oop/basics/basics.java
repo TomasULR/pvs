0d8c89ff905d90fdc76072b619dc7a889fc2bca8
tomas.ulrych <tomas.ulrych1@gmail.com>
2024-09-11 09:43:15 +0200

    sad

---
+++ src/oop/basics/basics.java
@@ -0,0 +1,16 @@
+package oop.basics;
+
+public class basics {
+    public static void main(String[] args) {
+        Cordinates cordinates = new Cordinates();
+    }
+}
+
+class Cordinates{
+    double x,y;
+
+    public Cordinates() {
+        this.x = x;
+        this.y = y;
+    }
+}
