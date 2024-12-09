6e43c7865b4d7c56ba6c9d89e91e171a85f78c03
tomas.ulrych <tomas.ulrych1@gmail.com>
2024-12-09 13:19:42 +0100

    sad

--- src/oop/fileworks/ReadingExamples.java
+++ src/oop/fileworks/ReadingExamples.java
@@ -3,6 +3,10 @@ package oop.fileworks;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 public class ReadingExamples {
     public static void main(String[] args) throws IOException {
@@ -11,9 +15,25 @@ public class ReadingExamples {
             FileReader reader = new FileReader(file);
             int input;
             while(((input = reader.read()) != -1)){
-                System.out.print((char) input);
+                //System.out.print((char) input);
             }
             reader.close();
         }
+        int vets =0;
+        int otazeks =0;
+        int vykricniks =0;
+        int slov =0;
+        List<String> lines = Files.readAllLines(Paths.get("inputs.txt"));
+        for (String line : lines){
+            for (int i = 0; i < line.length(); i++) {
+                if(line.charAt(i) == '.') vets++;
+                if(line.charAt(i) == ' ') slov++;
+                if(line.charAt(i) == '!') vykricniks++;
+                if(line.charAt(i) == '?') otazeks++;
+
+
+            }
+        }
+        System.out.println(vets + " " + otazeks + " " + vykricniks + " " + slov);
     }
 }
